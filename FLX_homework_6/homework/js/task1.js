0b112d6a7ddf33322240b9bd314bf014542e9d15
dodgecu <rogertaylor1949@gmail.com>
2019-02-03 15:19:34 +0200

    homework js

---
+++ FLX_homework_6/homework/js/task1.js
@@ -0,0 +1,27 @@
+function equate() {
+  const a = parseFloat(prompt("Please, specify A value", "0")),
+    b = parseFloat(prompt("Please, specify B value", "0")),
+    c = parseFloat(prompt("Please, specify C value", "0"));
+
+  const calculate = (a, b, c) => {
+    let result;
+    if (a === 0 || (isNaN(a) && isNaN(b) && isNaN(c))) {
+      result = "Invalid input data";
+    } else {
+      const discriminant = b * b - 4 * a * c;
+      if (discriminant === 0) {
+        result = `x = ${-b / (2 * a)}`;
+      } else if (discriminant > 0) {
+        const x1 = (-1 * b + Math.sqrt(discriminant)) / (2 * a);
+        const x2 = (-1 * b - Math.sqrt(discriminant)) / (2 * a);
+        result = `x1 = ${x1} and x2 = ${x2}`;
+      } else {
+        result = "no solution";
+      }
+    }
+    return result;
+  };
+
+  return calculate(a, b, c);
+}
+alert(equate());
---
+++ FLX_homework_6/homework/js/task2.js
@@ -0,0 +1,23 @@
+function calculateDiscount() {
+  const total = parseFloat(prompt("Please, specify the amount of money", "0"));
+  const discount = parseFloat(prompt("Please, specify the discount", "0"));
+
+  if (
+    !isNaN(total) &&
+    !isNaN(discount) &&
+    (total >= 0 && discount >= 0) &&
+    (total <= 9999999 && discount < 99)
+  ) {
+    const savings = (total / 100) * discount;
+    const discounted = total - savings;
+    return `
+        Price without discount: ${+total.toFixed(2)} 
+        Discount: ${+discount.toFixed(2)}%
+        Price with discount: ${+discounted.toFixed(2)}
+        Saved: ${+savings.toFixed(2)}
+  `;
+  } else {
+    return "Invalid input data";
+  }
+}
+alert(calculateDiscount());
